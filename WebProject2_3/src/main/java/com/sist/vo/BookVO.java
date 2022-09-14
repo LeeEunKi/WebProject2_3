afc16dfab3a427392670ee4a643d8164ac7e7e88
jiyun.kim <wldbs9644@gmail.com>
2022-09-14 17:23:46 +0900

    패키지(+BookVO, UsedBookVO), config 추가

---
+++ WebProject2_3/src/main/java/com/sist/vo/BookVO.java
@@ -0,0 +1,23 @@
+package com.sist.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.*;
+/*  이름                 NULL?       유형             
+----------- -------- -------------- 
+NO          NOT NULL NUMBER         
+TITLE       NOT NULL VARCHAR2(1000) 
+AUTHOR      NOT NULL VARCHAR2(1000) 
+TYPE        NOT NULL VARCHAR2(100)  
+PUBLISHER   NOT NULL VARCHAR2(500)  
+PRICE       NOT NULL NUMBER         
+IMG         NOT NULL VARCHAR2(1000) 
+PUB_DATE    NOT NULL DATE           
+DESCRIPTION          CLOB           */
+@Getter
+@Setter
+public class BookVO {
+	private int no, price;
+	private String title, author, type, publisher, img, description, dbday;
+	private Date pub_date;
+}
---
+++ WebProject2_3/src/main/java/com/sist/vo/UsedBookVO.java
@@ -0,0 +1,29 @@
+package com.sist.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.*;
+/* 이름                 NULL?       유형             
+----------- -------- -------------- 
+BOOK_NO              NUMBER         
+NO          NOT NULL NUMBER         
+CONDITION            VARCHAR2(100)  
+PRICE                NUMBER         
+REGDATE              DATE           
+STATE                NUMBER         
+IMG                  VARCHAR2(1000) 
+PUBLISHER            VARCHAR2(500)  
+AUTHOR               VARCHAR2(1000) 
+DISCOUNT             NUMBER         
+PUB_DATE             DATE           
+DESCRIPTION          CLOB           
+TYPE                 VARCHAR2(100)  
+TITLE                VARCHAR2(1000) */
+@Getter
+@Setter
+public class UsedBookVO {
+	private int book_no, no, price, state, discount;
+	private String title, author, type, publisher, img, description, dbday;
+	private String condition;
+	private Date pub_date;
+}

13e18fc278e89456886412e4f634392ee260c94e
LeeEunki <eklee0514@naver.com>
2022-09-28 15:53:38 +0900

    열람실 실시간 예약 취소222

--- WebProject2_3/src/main/java/com/sist/aop/StudyAspect.java
+++ WebProject2_3/src/main/java/com/sist/aop/StudyAspect.java
@@ -34,13 +34,18 @@ public class StudyAspect {
 			for(BookingVO vo:list) {
 				int res=vo.getDbday().compareTo(now_date);
 				int res2=vo.getEnd_time().compareTo(now_time);
-				
+				System.out.println("예약번호:"+vo.getNo());
+				System.out.println("예약날짜:"+vo.getDbday());
+				System.out.println("예약종료시간:"+vo.getEnd_time());
+				System.out.println("날짜비교:"+res);
+				System.out.println("시간비교:"+res2);
+				System.out.println("===============================");
 				if(vo.getDbday().compareTo(now_date)<0) {
 					dao.bookingCancel(vo);
 					System.out.println("전날 비교ok");
 				}
 				else if(vo.getDbday().compareTo(now_date)==0) {
-					if(vo.getEnd_time().compareTo(now_time)<=0) {
+					if(vo.getEnd_time().compareTo(now_time)<0) {
 						dao.bookingCancel(vo);
 						System.out.println("같은날 비교ok");
 					}
